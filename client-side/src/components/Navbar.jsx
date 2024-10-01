6ebcc81e30c95508885f83fd6979165b8fdc28cd
Modasser Hasan <manvstech2.0@gmail.com>
2024-10-01 23:54:20 +0600

    Navbar and Footer created

---
+++ client-side/src/components/Navbar.jsx
@@ -0,0 +1,121 @@
+import { Link, NavLink } from "react-router-dom";
+import UserDataContext from "../context/context";
+import { useContext } from "react";
+import { toast } from "react-toastify";
+
+const NavbarComponent = () => {
+    const context = useContext(UserDataContext);
+    const { userAuthData, dataLoading } = context;
+    console.log(userAuthData);
+
+    return (
+        <nav className="max-width bg-white dark:bg-gray-900 border-b border-gray-200 dark:border-gray-600 mb-4">
+            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                <Link
+                    to="/"
+                    className="flex items-center space-x-3 rtl:space-x-reverse"
+                >
+                    <img src="logo.png" className="h-8" alt="Blogify Logo" />
+                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+                        Blogify
+                    </span>
+                </Link>
+                <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+                    {dataLoading ? (
+                        <div className="loading"></div>
+                    ) : userAuthData ? (
+                        <div className="dropdown dropdown-end">
+                            <div
+                                tabIndex={0}
+                                role="button"
+                                className="btn btn-ghost btn-circle avatar"
+                            >
+                                <div className="w-10 rounded-full">
+                                    <img
+                                        alt="Tailwind CSS Navbar component"
+                                        src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                                    />
+                                </div>
+                            </div>
+                            <ul
+                                tabIndex={0}
+                                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+                            >
+                                <li>
+                                    <a className="justify-between">
+                                        Profile
+                                        <span className="badge">New</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a>Settings</a>
+                                </li>
+                                <li>
+                                    <a>Logout</a>
+                                </li>
+                            </ul>
+                        </div>
+                    ) : (
+                        <Link to={"/login"}>
+                            <button
+                                type="button"
+                                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                            >
+                                Login
+                            </button>
+                        </Link>
+                    )}
+                    <button
+                        data-collapse-toggle="navbar-sticky"
+                        type="button"
+                        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                        aria-controls="navbar-sticky"
+                        aria-expanded="false"
+                    >
+                        <span className="sr-only">Open main menu</span>
+                        <svg
+                            className="w-5 h-5"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 17 14"
+                        >
+                            <path
+                                stroke="currentColor"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M1 1h15M1 7h15M1 13h15"
+                            />
+                        </svg>
+                    </button>
+                </div>
+                <div
+                    className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+                    id="navbar-sticky"
+                >
+                    <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 [&_.active]:text-blue-700 [&_.active]:dark:text-blue-500">
+                        <li>
+                            <NavLink
+                                to="/"
+                                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                            >
+                                Home
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink
+                                to="/about"
+                                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                            >
+                                About
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default NavbarComponent;
--- client-side/src/main.jsx
+++ client-side/src/main.jsx
@@ -1,10 +1,213 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// React Router Dom
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import Login from "./routes/Login.jsx";
+// import Register from "./routes/Register.jsx";
+import PrivateRoute from "./components/PrivateRoute.jsx";
+import Home from "./routes/Home.jsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        // errorElement: <NotFound />,
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+                // loader: async () => [
+                //     await (
+                //         await fetch(
+                //             `${import.meta.env.VITE_SERVER_URL}/queries?limit=6`
+                //         )
+                //     ).json(),
+                //     await (
+                //         await fetch(
+                //             `${
+                //                 import.meta.env.VITE_SERVER_URL
+                //             }/tips-n-guides?limit=4`
+                //         )
+                //     ).json(),
+                // ],
+            },
+            // {
+            //     path: "/login",
+            //     element: (
+            //         <PrivateRoute reverse>
+            //             <Login />
+            //         </PrivateRoute>
+            //     ),
+            // },
+            // {
+            //     path: "/sign-up",
+            //     element: (
+            //         <PrivateRoute reverse>
+            //             <Register />
+            //         </PrivateRoute>
+            //     ),
+            // },
+            // {
+            //     path: "/update-profile",
+            //     element: (
+            //         <PrivateRoute>
+            //             <UpdateProfile />
+            //         </PrivateRoute>
+            //     ),
+            // },
+            // {
+            //     path: "/queries",
+            //     element: <Queries />,
+            //     loader: async () => [
+            //         await (
+            //             await fetch(
+            //                 `${import.meta.env.VITE_SERVER_URL}/queries?limit=9`
+            //             )
+            //         ).json(),
+            //         await (
+            //             await fetch(
+            //                 `${import.meta.env.VITE_SERVER_URL}/queries/count`
+            //             )
+            //         ).json(),
+            //     ],
+            // },
+            // {
+            //     path: "/query-details/:queryId",
+            //     element: <QueryDetails />,
+            //     loader: async ({ params }) => [
+            //         await (
+            //             await fetch(
+            //                 `${import.meta.env.VITE_SERVER_URL}/queries/${
+            //                     params.queryId
+            //                 }`
+            //             )
+            //         ).json(),
+            //         await (
+            //             await fetch(
+            //                 `${
+            //                     import.meta.env.VITE_SERVER_URL
+            //                 }/recommendations?queryId=${params.queryId}`
+            //             )
+            //         ).json(),
+            //     ],
+            // },
+            // {
+            //     path: "/me/recommendations-for-me",
+            //     element: (
+            //         <PrivateRoute>
+            //             <RecommendationsForMe />
+            //         </PrivateRoute>
+            //     ),
+            //     loader: async () => {
+            //         const res = await fetch(
+            //             `${
+            //                 import.meta.env.VITE_SERVER_URL
+            //             }/me/recommendations/for-me`,
+            //             {
+            //                 credentials: "include",
+            //             }
+            //         );
+            //         const data = await res.json();
+            //         if (data.success) {
+            //             return data.result;
+            //         } else {
+            //             await logoutUser();
+            //             return null;
+            //         }
+            //     },
+            // },
+            // {
+            //     path: "/me/queries",
+            //     element: (
+            //         <PrivateRoute>
+            //             <MyQueries />
+            //         </PrivateRoute>
+            //     ),
+            //     loader: async () => {
+            //         const res = await fetch(
+            //             `${import.meta.env.VITE_SERVER_URL}/me/queries`,
+            //             {
+            //                 credentials: "include",
+            //             }
+            //         );
+            //         const data = await res.json();
+            //         if (data.success) {
+            //             return data.result;
+            //         } else {
+            //             await logoutUser();
+            //             return null;
+            //         }
+            //     },
+            // },
+            // {
+            //     path: "/me/recommendations",
+            //     element: (
+            //         <PrivateRoute>
+            //             <MyRecommendations />
+            //         </PrivateRoute>
+            //     ),
+            //     loader: async () => {
+            //         const res = await fetch(
+            //             `${import.meta.env.VITE_SERVER_URL}/me/recommendations`,
+            //             {
+            //                 credentials: "include",
+            //             }
+            //         );
+            //         const data = await res.json();
+            //         if (data.success) {
+            //             return data.result;
+            //         } else {
+            //             await logoutUser();
+            //             return null;
+            //         }
+            //     },
+            // },
+            // {
+            //     path: "/create-query",
+            //     element: (
+            //         <PrivateRoute>
+            //             <EditQuery />
+            //         </PrivateRoute>
+            //     ),
+            // },
+            // {
+            //     path: "/edit-query/:queryId",
+            //     element: (
+            //         <PrivateRoute>
+            //             <EditQuery />
+            //         </PrivateRoute>
+            //     ),
+            //     loader: async ({ params }) => {
+            //         const res = await fetch(
+            //             `${import.meta.env.VITE_SERVER_URL}/queries/${
+            //                 params.queryId
+            //             }`,
+            //             {
+            //                 credentials: "include",
+            //             }
+            //         );
+            //         const data = await res.json();
+            //         if (
+            //             res.status === 200 ||
+            //             res.status === 404 ||
+            //             res.status === 400
+            //         ) {
+            //             return data;
+            //         } else {
+            //             await logoutUser();
+            //             return null;
+            //         }
+            //     },
+            // },
+        ],
+    },
+]);
+
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>
+);
